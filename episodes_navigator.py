3742dba8e1f9b90555a54e92ee2bc4e97901bfc6
adavideo <adavideo@gmail.com>
2023-08-18 13:33:37 +0200

    Shows the episode information after selecting it

--- episodes_navigator.py
+++ episodes_navigator.py
@@ -1,3 +1,10 @@
+from manage_storage import get_episode_tittle, get_episode_summary
+
+def show_episode_information(episode_number):
+    episode_tittle = get_episode_tittle(episode_number)
+    episode_summary = get_episode_summary(episode_number)
+    print("Episode "+str(episode_number) + ": " + episode_tittle)
+    print("Summary: " + episode_summary)
 
 def select_episode():
     episode_number = ""
@@ -7,4 +14,5 @@ def select_episode():
             episode_number = int(user_input)
         else:
             user_input = input("On what episode are we going to work? Please write a number: ")
+    show_episode_information(episode_number)
     return episode_number
--- manage_storage.py
+++ manage_storage.py
@@ -10,3 +10,15 @@ def get_tematic():
     data = load_json("config")
     tematic = data["tematic"]
     return tematic
+
+def get_episode_tittle(episode_number):
+    episodes_list = load_json("episodes_list")
+    episode_information = episodes_list["episodes"][episode_number-1]
+    title = episode_information["title"]
+    return title
+
+def get_episode_summary(episode_number):
+    episodes_list = load_json("episodes_list")
+    episode_information = episodes_list["episodes"][episode_number-1]
+    summary = episode_information["summary"]
+    return summary
